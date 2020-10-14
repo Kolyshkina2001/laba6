3d810268fe14824df252d7864aa73636cf346f30
yakol <ya.kolysh@gmail.com>
2020-10-14 18:30:49 +0300

    Initial commit

---
+++ src/ru/mirea/Yana/laba6/StudentComparator.java
@@ -0,0 +1,9 @@
+package ru.mirea.Yana.laba6;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<Student> {
+    public int compare(Student o1, Student o2) {
+        return o1.getSumOrder() - o2.getSumOrder();
+    }
+}
